1e91355625a824ed02299d93d85620b2017b2b95
Vitor basseto <vitorhugobassetto@gmail.com>
2023-05-09 22:17:17 -0300

    react-native init

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
 
 export default function App() {
+  var plat = "tested";
+  if (Platform.OS === "android") {
+    plat = "Android";
+  } else if (Platform.OS === "ios") {
+    plat = "IOS";
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{plat } teste</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
